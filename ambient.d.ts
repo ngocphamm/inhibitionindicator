fdbb4d57b68029908ea8fc3a02aba0628f78f61b
Max Xie <monyxie@gmail.com>
2025-08-06 19:14:19 +0800

    Add @gjs type defs for better dev experience

---
+++ ambient.d.ts
@@ -0,0 +1,8 @@
+import "@girs/adw-1";
+import "@girs/gio-2.0";
+import "@girs/gtk-4.0";
+import "@girs/gjs";
+import "@girs/gjs/dom";
+import "@girs/gnome-shell/ambient";
+import "@girs/gnome-shell/extensions/global";
+import "@girs/gnome-shell/extensions/prefs";
